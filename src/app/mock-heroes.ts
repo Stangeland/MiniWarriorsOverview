fac3ab738bd968684021189f3e3e3cd39563c813
Stangeland <Stangeland@users.noreply.github.com>
2019-08-19 18:38:36 +0200

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+export const HEROES: Hero[]=[
+    {id: 11, name: 'El Cid'},
+    {id: 12, name: 'Einar'},
+    {id: 13, name: 'Hilda'},
+    {id: 14, name: 'Merlin'},
+    {id: 15, name: 'Terim'},
+    {id: 16, name: 'Ignatius'},
+    {id: 17, name: 'Achilles'},
+    {id: 18, name: 'Belthor'},
+    {id: 19, name: 'Galahad'},
+    {id: 20, name: 'Laverty'}
+]
